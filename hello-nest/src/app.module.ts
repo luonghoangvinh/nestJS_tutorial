7533413a93d0a0b58d994787c0fedbfea7c3fc71
lhvinh <lhvinh2005@gmail.com>
2026-07-30 16:25:02 +0700

    setup i18n + setup swagger

--- hello-nest/src/app.module.ts
+++ hello-nest/src/app.module.ts
@@ -2,11 +2,29 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { Tutorial1Module } from './tutorial1/tutorial1.module';
-
+import { I18nModule, QueryResolver } from 'nestjs-i18n';
+import * as path from 'node:path';
 
 @Module({
-  imports: [Tutorial1Module],
+  imports: [
+    I18nModule.forRoot({
+      fallbackLanguage: 'vi',
+
+      loaderOptions: {
+        path: path.join(__dirname, '../i18n'),
+        watch: true,
+      },
+
+      resolvers: [
+        {
+          use: QueryResolver,
+          options: ['lang'],
+        },
+      ],
+    }),
+    Tutorial1Module
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
