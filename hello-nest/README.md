7a445bedacc933d50bd434816e32552a317fcfa9
lhvinh <lhvinh2005@gmail.com>
2026-07-30 16:37:58 +0700

    thêm chi tiết readme và swagger

--- hello-nest/README.md
+++ hello-nest/README.md
@@ -3,3 +3,7 @@ dùng swagger để kiểm tra api
 B1: chạy npm run start:dev
 B2: vào http://localhost:3000/api
 
+api hello world
+http://localhost:3000/ - mặc định là tiếng việt
+http://localhost:3000/?lang=en - tiếng anh
+http://localhost:3000/?lang=vi - tiếng việt
--- hello-nest/src/app.controller.ts
+++ hello-nest/src/app.controller.ts
@@ -2,12 +2,24 @@ import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
 import { I18n, I18nContext } from 'nestjs-i18n';
+import { ApiOperation, ApiQuery } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({
+  summary: 'Hello World',
+  description: 'Trả về lời chào theo ngôn ngữ được chọn(vi hoặc en).',
+})
+@ApiQuery({
+    name: 'lang',
+    required: false,
+    example: 'vi',
+    enum: ['vi', 'en'],
+    description: 'Language'
+})
   getHello(@I18n() i18n: I18nContext) {
     return this.appService.getHello(i18n);
   }
