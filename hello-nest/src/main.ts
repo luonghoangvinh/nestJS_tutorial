bca45f1477fc6356289d158b7e8a915853276763
lhvinh <lhvinh2005@gmail.com>
2026-07-31 07:57:38 +0700

    bổ sung readme

--- hello-nest/src/main.ts
+++ hello-nest/src/main.ts
@@ -5,7 +5,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  const PORT = 3000;
   const config = new DocumentBuilder()
     .setTitle('Hello Nest API')
     .setDescription('API Documentation')
@@ -16,7 +16,7 @@ async function bootstrap() {
 
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(PORT);
 }
 
 bootstrap();
